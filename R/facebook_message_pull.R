47701cd6b8cf681053ccabd7ecebef52f3774bdb
gonzalezben81 <gonzalezben81@gmail.com>
2019-08-15 22:02:04 +0000

    Updated message pull to create an xlsx workbook that has the Sender, Date and Time, and the Message content and writes it to the xlsx file.

--- R/facebook_message_pull.R
+++ R/facebook_message_pull.R
@@ -13,7 +13,8 @@
 #'
 #' This function pulls the sentences from your messages folder and places them in a handy .csv file for analysis.
 #' The participants, timestamp, and content of the messages are pulled out and placed in the .csv file that is created.
-#' The .csv file is names for the participant you have been messaging.
+#' The .csv file is names for the participant you have been messaging. The function also pulls the date and time from the Json file
+#' and converts it and writes the Sender, Time, and Content of the message to an .xlsx file.
 #'
 #'
 #' @param folder filepath to data
@@ -37,7 +38,7 @@
 
 
 facebook_message_pull <- function(folder){
-
+  library(openxlsx)
 
   filelist <- list.files(path = folder, full.names = TRUE)
 
@@ -58,17 +59,18 @@ facebook_message_pull <- function(folder){
     # text$participants[2,1]
     # message_frame<- as.data.frame(text_frame$messages)
 
-    text_sender<- text$messages$sender_name[1]
+    text_sender<- text$messages$sender_name
     text_sender_two<- text$messages$sender_name[2]
-    print(paste0('Text Sender one',text_sender))
-    print(paste0('Text Sender two',text_sender_two))
+    text_time<- as.list(head(as.POSIXct(as.integer(as.numeric(as.character(text$messages$timestamp_ms)) / 1000.0),
+                                        origin='1970-01-01', tz="UTC")))
+
     text_content <- text$messages$content
     unique_sender<- unique(text_sender)
     name<- unique(text_sender)
 
     text_content <- text$messages$content
 
-    text_all <- cbind(text_sender,text_content)
+    text_all <- cbind(text_sender,text_time,text_content)
 
     unique_sender_one <- text_all[ which(text_all[,1]==unique_sender[1]), ]
     # unique_sender_one <- as.data.frame(unique_sender_one)
@@ -85,8 +87,15 @@ facebook_message_pull <- function(folder){
     }else{
       name<- gsub(pattern = './facebookmessages/messages/inbox/',replacement = "",x = f)
       name <- gsub(pattern = "_.*",replacement = "",x = name)
-      write.table(x = message_content,file = paste0('./messages/',name,'.txt'))
-      # write.csv(x = unique_sender_one,file = paste0('./messages/',name,'_sentences.csv'))
+      write.table(x = text_all,file = paste0('./messages/',name,'.txt'))
+      write.csv(x = text_all,file = paste0('./messages/',name,'_sentences.csv'))
+      wb <- createWorkbook()
+      addWorksheet(wb, paste0("Facebook Data"), gridLines = TRUE)
+
+      sty <- createStyle(numFmt = "yyyy/mm/dd hh:mm:ss")
+      addStyle(wb = wb,sheet =  1, style = sty,rows = 1, cols = 1, gridExpand = TRUE)
+      writeData(wb = wb, "Facebook Data", text_all, startRow = 1,startCol = 1)
+      saveWorkbook(wb, file = paste0("./messages/FacebookDataDownload",name,".xlsx"), overwrite = TRUE)
       # write.csv(x = unique_sender_two,file = paste0('./messages/',name,'_',text_sender_two,'.csv'))
     }
 
